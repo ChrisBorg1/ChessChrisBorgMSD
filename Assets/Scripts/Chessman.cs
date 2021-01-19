ce91bc5ebd0a98e2a0244ef3b09192e12561a438
ChrisBorg1 <51298928+ChrisBorg1@users.noreply.github.com>
2021-01-19 17:39:52 +0100

    Chess piece and sprites
    Added a chess piece and created sprites for each chess(Not assigned yet). Create a new script called Chessman(for the chess pieces/sprites)

---
+++ Assets/Scripts/Chessman.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chessman : MonoBehaviour
+{
+  //References
+    public GameObject controller;
+    public GameObject movePlate;
+
+  //Positions
+  private int xBoard = -1;
+  private int yBoard = -1;
+
+  //Variable to keep track of player 1 and player 2
+  private string player;
+
+  //References for all the sprites that the chess piece can be
+  public Sprite black_queen, black_knight, black_bishop, black_king, black_rook, black_pawn;
+public Sprite white_queen, white_knight, white_bishop, white_king, white_rook, white_pawn;
+
+}
