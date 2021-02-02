a4f56cd6107ed8d8958bf0d6f261e97fb36bdcab
ChrisBorg1 <51298928+ChrisBorg1@users.noreply.github.com>
2021-02-02 21:58:40 +0100

    Added input field for Email and Password
    Players can now create an account with en e-mail and password and can then log in. If the credentials are correct then the players can play the game

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-
 public class Game : MonoBehaviour
 {
     //Reference from Unity IDE
