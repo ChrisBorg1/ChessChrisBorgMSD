e4ad2eae0f861c18cbb781f22da0b13c01fd6ee8
ChrisBorg1 <51298928+ChrisBorg1@users.noreply.github.com>
2021-02-07 14:50:30 +0100

    Progress Bar added
    Added Progress Bar in Game Scene

---
+++ Assets/Scripts/ProgressBar.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBar : MonoBehaviour
+{
+
+    public GameObject progressBar;
+    private Slider slider;
+    public float MaxTime = 6.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Awake()
+    {
+        slider = gameObject.GetComponent<Slider>();
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+          MaxTime -= Time.deltaTime;
+        if (MaxTime < 5f)
+        {
+            if (slider.value < 1)
+            {
+                slider.value += 0.25f * Time.deltaTime;
+            }
+            if (slider.value == slider.maxValue)
+            {
+               this.gameObject.SetActive(false);
+            }
+        }
+    }
+}
